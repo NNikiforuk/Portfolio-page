4ee29755d65c3e4cc4a99a8460e7d3477d7c751f
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-12 00:12:56 +0200

    Updating list of projects

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,4 +1,5 @@
 import Pro1 from "../assets/jpw.png";
+import Pro2 from "../assets/monthlymodifiers.png";
 
 export const projectList = [
 	{
@@ -8,6 +9,17 @@ export const projectList = [
 		skills: ["Astro", "TypeScript", "Web3Forms", "SCSS"],
 		github: "https://github.com/NNikiforuk/Jezykowy-Punkt-Widzenia-version-4",
 		deploy: "https://www.jpw-kursy.pl/",
-		functionalities: ["Commercial website for polish language school"],
+		functionalities: ["Commercial website for polish language school."],
+	},
+	{
+		name: "Monthly Modifiers",
+		key: 2,
+		image: Pro2,
+		skills: ["Astro", "TypeScript", "SCSS", "NIST Randomness Beacon"],
+		github: "https://github.com/NNikiforuk/Monthly-Modifiers",
+		deploy: "https://monthlymodifiers.com/",
+		functionalities: [
+			"Each month a challenge is drawn using the NIST Randomness Beacon. Choose a challenge or choose them all and start testing yourself.",
+		],
 	},
 ];
